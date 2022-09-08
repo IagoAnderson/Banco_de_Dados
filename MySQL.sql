2f2a131d8047c69c83935943f7357668e2da3749
IagoAnderson <83406071+IagoAnderson@users.noreply.github.com>
2022-09-08 16:47:29 -0300

    Update MySQL.sql

--- MySQL.sql
+++ MySQL.sql
@@ -1,9 +1,18 @@
 
 CREATE TABLE `Produtos` (
-    `CodProduto` INT NOT NULL,
-    `Descricao` VARCHAR(40000) NOT NULL,
-    `Marca` VARCHAR(40000) NULL,
-    `Preco` DOUBLE(10000000000,2) NOT NULL,
-    `Tipo`  
+    `CodProduto` INT NOT NULL COMMENT"Código do produto",
+    `Descricao` VARCHAR(40000) NOT NULL COMMENT"Nome do produto",
+    `Marca` VARCHAR(40000) NULL COMMENT"Marca do produto",
+    `Preco` DOUBLE(10000000000,2) NOT NULL COMMENT"Preço do produto" ,
+    `Tipo` VARCHAR(40000) NOT NULL COMMENT"Tipo do produto",
+);
 
+CREATE TABLE `Moveis` (
+    `CodEletro` INT NOT NULL COMMENT"Código do Móvel",
+    `Cor` VARCHAR(400) NOT NULL COMMENT"Cor do Móvel",
+    `Material` VARCHAR(400) NOT NULL COMMENT"Material de que é feito"
+);
+
+CREATE TABLE `Eletronicos` (
+    `CodEletro`
 );
