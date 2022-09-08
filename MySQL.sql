424ee1666652146ba95f38e1035585e41e8c1b99
IagoAnderson <83406071+IagoAnderson@users.noreply.github.com>
2022-09-08 17:02:45 -0300

    Update MySQL.sql

--- MySQL.sql
+++ MySQL.sql
@@ -1,18 +1,25 @@
 
 CREATE TABLE `Produtos` (
-    `CodProduto` INT NOT NULL COMMENT"Código do produto",
+    `CodProduto` INT NOT NULL COMMENT"Código do produto" ,
     `Descricao` VARCHAR(40000) NOT NULL COMMENT"Nome do produto",
     `Marca` VARCHAR(40000) NULL COMMENT"Marca do produto",
     `Preco` DOUBLE(10000000000,2) NOT NULL COMMENT"Preço do produto" ,
     `Tipo` VARCHAR(40000) NOT NULL COMMENT"Tipo do produto",
-);
+    PRIMARY KEY(`CodProduto`)
+    );
 
 CREATE TABLE `Moveis` (
     `CodEletro` INT NOT NULL COMMENT"Código do Móvel",
     `Cor` VARCHAR(400) NOT NULL COMMENT"Cor do Móvel",
-    `Material` VARCHAR(400) NOT NULL COMMENT"Material de que é feito"
-);
+    `Material` VARCHAR(400) NOT NULL COMMENT"Material de que é feito",
+    PRIMARY KEY(`CodEletro`),
+    CONSTRAINT ``
+    );
 
 CREATE TABLE `Eletronicos` (
-    `CodEletro`
-);
+    `CodEletro` INT NOT NULL COMMENT "Código do Eletrodoméstico",
+    `Voltagem` INT NOT NULL COMMENT "Voltagem do Eletrodoméstico",
+    `Garantia` INT NULL COMMENT"Tempo de validade da garantia (em anos)"
+    `CodBarra` VARCHAR(400000) NOT NULL COMMENT"Código de barra do eletrodoméstico",
+    PRIMARY KEY(`CodEletro`)
+    );
