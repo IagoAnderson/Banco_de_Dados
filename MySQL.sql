305e2b0335fab96046f4ebd8db3051c1f3574997
IagoAnderson <83406071+IagoAnderson@users.noreply.github.com>
2022-09-08 16:40:21 -0300

    Add files via upload

---
+++ MySQL.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE `Produtos` (
+    `CodProduto` INT NOT NULL,
+    `Descricao` VARCHAR(40000) NOT NULL,
+    `Marca` VARCHAR(40000) NULL,
+    `Preco` DOUBLE(10000000000,2) NOT NULL,
+    `Tipo`  
+
+);
